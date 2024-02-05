edd3c53335254fffdcd7d832903c7979de015bb6
rit245 <urit245@gmail.com>
2024-02-06 02:26:09 +0900

    11. 한글화 추가

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -8,12 +8,12 @@
         <form action="/articles" method="post" class="mt-3">
             @csrf
             <input type="text" name="body" class="block w-full mb-2 rounded">
-            <button class="py-1 px-3 bg-black text-white rounded text-xs">
-                저장하기
-            </button>
             @error('body')
             <p class="text-xs text-red-500 mb-3"> {{ $message }} </p>
             @enderror
+            <button class="py-1 px-3 bg-black text-white rounded text-xs">
+                저장하기
+            </button>
 {{--            {{ dd($errors) }}--}}
 {{--            {{ dd($errors->all) }}--}}
 {{--            {{ dd($errors->any) }}--}}
